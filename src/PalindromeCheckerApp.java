48ab1fce0f6fc25b81b91b4963f9abdcdc45b0e6
Nithilan02 <nithilan.sivakumar2006@gmail.com>
2026-02-26 15:27:25 +0530

    UC3: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,21 +2,22 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string
-        String word = "madam";
+        // Original String
+        String original = "level";
 
-        // Reverse logic
+        // Variable to store reversed string
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        // Reverse using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        // Palindrome check
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome");
+        // Compare using equals()
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
         } else {
-            System.out.println(word + " is Not a Palindrome");
+            System.out.println(original + " is Not a Palindrome");
         }
     }
 }
