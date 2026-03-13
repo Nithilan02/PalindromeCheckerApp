bc0faff9d99cdd1f55e7e41148aa8fbc653538b3
Nithilan02 <nithilan.sivakumar2006@gmail.com>
2026-03-13 15:40:22 +0530

    UC8: Linked list based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,83 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
+    // Node class for Linked List
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Function to check palindrome using linked list
+    public static boolean isPalindrome(String str) {
+
+        if (str == null || str.length() == 0)
+            return true;
+
+        // Step 1: Convert string to linked list
+        Node head = new Node(str.charAt(0));
+        Node current = head;
+
+        for (int i = 1; i < str.length(); i++) {
+            current.next = new Node(str.charAt(i));
+            current = current.next;
+        }
+
+        // Step 2: Find middle using fast & slow pointer
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 3: Reverse second half
+        Node prev = null;
+        Node next;
+
+        while (slow != null) {
+            next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        // Step 4: Compare halves
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Welcome to Palindrome Checker App");
+
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+
+        sc.close();
     }
 }
